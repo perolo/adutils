95d636c481bf9045491e1011bfc2f9e0a70b4be8
Lenovo <per.e.olofsson@gmail.com>
2022-05-30 16:42:56 +0200

    Migrate to confluence-go-api

--- go.mod
+++ go.mod
@@ -4,29 +4,30 @@ go 1.18
 
 require (
 	github.com/magiconair/properties v1.8.6
-	github.com/perolo/confluence-client v0.0.0-20220408191902-81a41e5cb047
-	github.com/perolo/confluence-scripts v0.0.0-20220412080954-25f864854e75
-	github.com/perolo/excel-utils v0.0.0-20220404123251-ae7963967236
+	github.com/perolo/confluence-go-api v1.3.1-0.20220530143507-c3456d70cc91
+	github.com/perolo/excellogger v0.0.0-20220502103720-bdfef1a3d6a5
 	github.com/perolo/jira-client v0.0.0-20220410122149-d2c3be3e4688
-	github.com/perolo/ldap.v2 v0.0.0-20220412075522-5a98471c017d
+	gopkg.in/ldap.v2 v2.5.1
 )
 
 require (
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/magefile/mage v1.13.0 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
-	github.com/perolo/jira-scripts v0.0.0-20220411180542-987c6aabe088 // indirect
-	github.com/perolo/myexcelize v1.4.2-0.20220404083812-f49112898c31 // indirect
+	github.com/perolo/gojson v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.1 // indirect
 	github.com/trivago/tgo v1.0.7 // indirect
-	github.com/xuri/efp v0.0.0-20220216053911-6d8731f62184 // indirect
-	github.com/xuri/nfp v0.0.0-20220215121256-71f1502108b5 // indirect
-	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
+	github.com/xuri/efp v0.0.0-20220407160117-ad0f7a785be8 // indirect
+	github.com/xuri/excelize/v2 v2.6.1-0.20220429055309-0f93bd23c97a // indirect
+	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
+	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a // indirect
 	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/asn1-ber.v1 v1.0.0-20181015200546-f715ec2f112d // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- syncadgroup/syncadgroup.go
+++ syncadgroup/syncadgroup.go
@@ -3,14 +3,13 @@ package syncadgroup
 import (
 	"flag"
 	"fmt"
-	"github.com/magiconair/properties"
-	"github.com/perolo/ad-utils"
-	"github.com/perolo/confluence-client/client"
-	"github.com/perolo/confluence-scripts/utilities"
-	excelutils "github.com/perolo/excel-utils"
 	"log"
-	"path/filepath"
 	"time"
+
+	"github.com/magiconair/properties"
+	adutils "github.com/perolo/ad-utils"
+	goconfluence "github.com/perolo/confluence-go-api"
+	"github.com/perolo/excellogger"
 )
 
 type Config struct {
@@ -36,63 +35,80 @@ type Config struct {
 
 func initReport(cfg Config) {
 	if cfg.Report {
-		excelutils.NewFile()
-		excelutils.SetCellFontHeader()
-		excelutils.WiteCellln("Introduction")
-		excelutils.WiteCellln("Please Do not edit this page!")
-		excelutils.WiteCellln("This page is created by the projectreport script: github.com\\perolo\\ad-utils\\SyncADGroup")
+		excellogger.NewFile(nil)
+		excellogger.SetCellFontHeader()
+		excellogger.WiteCellln("Introduction")
+		excellogger.WiteCellln("Please Do not edit this page!")
+		excellogger.WiteCellln("This page is created by the projectreport script: github.com\\perolo\\ad-utils\\SyncADGroup")
 		t := time.Now()
-		excelutils.WiteCellln("Created by: " + cfg.ConfUser + " : " + t.Format(time.RFC3339))
-		excelutils.WiteCellln("")
-		excelutils.WiteCellln("The Report Function shows:")
-		excelutils.WiteCellln("   Ad Names 1- Name and user found in AD Group 1")
-		excelutils.WiteCellln("   Ad Names 2- Name and user found in AD Group 2")
-		excelutils.WiteCellln("   Not in AD group 1 - Users in the group 2 not found in group 1")
-		excelutils.WiteCellln("   Not in AD group 2 - Users in the group 1 not found in group 2")
-		excelutils.WiteCellln("   Not in JIRA - Users in the AD not found in the JIRA Group")
-		excelutils.WiteCellln("   AD Errors - Internal error when searching for user in AD")
-		excelutils.WiteCellln("")
-		excelutils.SetCellFontHeader2()
-		excelutils.WiteCellln("Group Mapping")
-		excelutils.WriteColumnsHeaderln([]string{"AD Group 1", "AD Group 2", "Ad Count 1", "Ad Count 2"})
+		excellogger.WiteCellln("Created by: " + cfg.ConfUser + " : " + t.Format(time.RFC3339))
+		excellogger.WiteCellln("")
+		excellogger.WiteCellln("The Report Function shows:")
+		excellogger.WiteCellln("   Ad Names 1- Name and user found in AD Group 1")
+		excellogger.WiteCellln("   Ad Names 2- Name and user found in AD Group 2")
+		excellogger.WiteCellln("   Not in AD group 1 - Users in the group 2 not found in group 1")
+		excellogger.WiteCellln("   Not in AD group 2 - Users in the group 1 not found in group 2")
+		excellogger.WiteCellln("   Not in JIRA - Users in the AD not found in the JIRA Group")
+		excellogger.WiteCellln("   AD Errors - Internal error when searching for user in AD")
+		excellogger.WiteCellln("")
+		excellogger.SetCellFontHeader2()
+		excellogger.WiteCellln("Group Mapping")
+		excellogger.WriteColumnsHeaderln([]string{"AD Group 1", "AD Group 2", "Ad Count 1", "Ad Count 2"})
 		if cfg.Simple {
-			excelutils.WriteColumnsln([]string{cfg.AdGroup, cfg.Localgroup})
+			excellogger.WriteColumnsln([]string{cfg.AdGroup, cfg.Localgroup})
 		} else {
 			for _, syn := range GroupSyncs {
-				excelutils.WriteColumnsln([]string{syn.AdGroup1, syn.AdGroup2})
+				excellogger.WriteColumnsln([]string{syn.AdGroup1, syn.AdGroup2})
 			}
 		}
-		excelutils.WiteCellln("")
-		excelutils.SetCellFontHeader2()
-		excelutils.WiteCellln("Report")
-		excelutils.AutoFilterStart()
+		excellogger.WiteCellln("")
+		excellogger.SetCellFontHeader2()
+		excellogger.WiteCellln("Report")
+		excellogger.AutoFilterStart()
 		var headers = []string{"Report Function", "AD group 1", "Local Group", "Name", "Uname", "Mail", "Error", "DN"}
-		excelutils.WriteColumnsHeaderln(headers)
+		excellogger.WriteColumnsHeaderln(headers)
 	}
 }
 
 func endReport(cfg Config) error {
 	if cfg.Report {
 		file := fmt.Sprintf(cfg.File, "-ADCompare")
-		excelutils.SetColWidth("A", "A", 60)
-		excelutils.AutoFilterEnd()
-		excelutils.SaveAs(file)
+		excellogger.SetColWidth("A", "A", 60)
+		excellogger.AutoFilterEnd()
+		excellogger.SaveAs(file)
 		if cfg.ConfUpload {
-			var config = client.ConfluenceConfig{}
-			var copt client.OperationOptions
-			config.Username = cfg.ConfUser
-			config.Password = cfg.ConfPass
-			config.UseToken = cfg.UseToken
-			config.URL = cfg.ConfHost
-			config.Debug = false
-			confluenceClient := client.Client(&config)
-			// Intentional override
-			copt.Title = "Using AD groups for JIRA/Confluence"
-			copt.SpaceKey = "AAAD"
-			_, name := filepath.Split(file)
-			cfg.ConfAttName = name
-			return utilities.AddAttachmentAndUpload(confluenceClient, copt, name, file, "Created by Sync AD group")
+			/*
+				var config = client.ConfluenceConfig{}
+				var copt client.OperationOptions
+				config.Username = cfg.ConfUser
+				config.Password = cfg.ConfPass
+				config.UseToken = cfg.UseToken
+				config.URL = cfg.ConfHost
+				config.Debug = false
+				confluenceClient := client.Client(&config)
+			*/
+			var confClient *goconfluence.API
+			var err error
+			if cfg.UseToken {
+				confClient, err = goconfluence.NewAPI(cfg.ConfHost, "", cfg.ConfToken)
+			} else {
+				confClient, err = goconfluence.NewAPI(cfg.ConfHost, cfg.ConfUser, cfg.ConfPass)
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			//	confClient.Debug = true
+			err = confClient.UppdateAttachment("AAAD", "Using AD groups for JIRA/Confluence", file)
 
+			// Intentional override
+			/*
+				copt.Title = "Using AD groups for JIRA/Confluence"
+				copt.SpaceKey = "AAAD"
+				_, name := filepath.Split(file)
+				cfg.ConfAttName = name
+				return utilities.AddAttachmentAndUpload(confluenceClient, copt, name, file, "Created by Sync AD group")
+			*/
+			return err
 		}
 	}
 	return nil
@@ -106,21 +122,21 @@ func AdSyncAdGroup(propPtr string) {
 	if err := p.Decode(&cfg); err != nil {
 		log.Fatal(err)
 	}
-	toolClient := toollogin(cfg)
+	//toolClient := toollogin(cfg)
 	initReport(cfg)
 	adutils.InitAD(cfg.Bindusername, cfg.Bindpassword)
 	x := 15
 	if cfg.Simple {
-		SyncGroupInTool(cfg, toolClient)
+		SyncGroupInTool(cfg)
 	} else {
 		for _, syn := range GroupSyncs {
 			AdCount := 0
 			GroupCount := 0
 			cfg.AdGroup = syn.AdGroup1
 			cfg.Localgroup = syn.AdGroup2
-			AdCount, GroupCount = SyncGroupInTool(cfg, toolClient)
-			excelutils.SetCell(fmt.Sprintf("%v", AdCount), 5, x)
-			excelutils.SetCell(fmt.Sprintf("%v", GroupCount), 6, x)
+			AdCount, GroupCount = SyncGroupInTool(cfg)
+			excellogger.SetCell(fmt.Sprintf("%v", AdCount), 5, x)
+			excellogger.SetCell(fmt.Sprintf("%v", GroupCount), 6, x)
 			x = x + 1
 		}
 	}
@@ -131,17 +147,7 @@ func AdSyncAdGroup(propPtr string) {
 	adutils.CloseAD()
 }
 
-func toollogin(cfg Config) *client.ConfluenceClient {
-	var config = client.ConfluenceConfig{}
-	config.Username = cfg.ConfUser
-	config.Password = cfg.ConfPass
-	config.UseToken = cfg.UseToken
-	config.URL = cfg.ConfHost
-	config.Debug = false
-	return client.Client(&config)
-}
-
-func SyncGroupInTool(cfg Config, client *client.ConfluenceClient) (adcount int, localcount int) {
+func SyncGroupInTool(cfg Config) (adcount int, localcount int) {
 	var toolGroupMemberNames map[string]adutils.ADUser
 	fmt.Printf("\n")
 	fmt.Printf("SyncGroup AdGroup: %s LocalGroup: %s \n", cfg.AdGroup, cfg.Localgroup)
@@ -158,7 +164,7 @@ func SyncGroupInTool(cfg Config, client *client.ConfluenceClient) (adcount int, 
 		if !cfg.Limited {
 			for _, adu := range adUnames1 {
 				var row = []string{"AD Names  1 ", cfg.AdGroup, cfg.Localgroup, adu.Name, adu.Uname, adu.Mail, adu.Err, adu.DN}
-				excelutils.WriteColumnsln(row)
+				excellogger.WriteColumnsln(row)
 			}
 		}
 		adcount = len(adUnames1)
@@ -171,7 +177,7 @@ func SyncGroupInTool(cfg Config, client *client.ConfluenceClient) (adcount int, 
 		if !cfg.Limited {
 			for _, adu2 := range adUnames2 {
 				var row = []string{"AD Names  2 ", cfg.AdGroup, cfg.Localgroup, adu2.Name, adu2.Uname, adu2.Mail, adu2.Err, adu2.DN}
-				excelutils.WriteColumnsln(row)
+				excellogger.WriteColumnsln(row)
 				toolGroupMemberNames[adu2.Uname] = adu2
 			}
 		}
@@ -191,7 +197,7 @@ func SyncGroupInTool(cfg Config, client *client.ConfluenceClient) (adcount int, 
 		if cfg.Report {
 			for _, nji := range notInTool {
 				var row = []string{"AD group 1 users not found Not In AD Group 2 group", cfg.AdGroup, cfg.Localgroup, nji.Name, nji.Uname, nji.Mail, nji.Err, nji.DN}
-				excelutils.WriteColumnsln(row)
+				excellogger.WriteColumnsln(row)
 			}
 		}
 		notInAD := adutils.Difference2(toolGroupMemberNames, adUnames1)
@@ -226,7 +232,7 @@ func SyncGroupInTool(cfg Config, client *client.ConfluenceClient) (adcount int, 
 								nad.Err = edn[0].Err
 								for _, ldn := range edn {
 									var row2 = []string{"Not In AD Group 1 user not found in AD group 2)", cfg.AdGroup, cfg.Localgroup, nad.Name, nad.Uname, ldn.Mail, ldn.Err, ldn.DN}
-									excelutils.WriteColumnsln(row2)
+									excellogger.WriteColumnsln(row2)
 								}
 							} else {
 
@@ -237,7 +243,7 @@ func SyncGroupInTool(cfg Config, client *client.ConfluenceClient) (adcount int, 
 
 				}
 				var row = []string{"AD group 2 users not found Not In AD Group 1 group", cfg.AdGroup, cfg.Localgroup, nad.Name, nad.Uname, nad.Mail, nad.Err, nad.DN}
-				excelutils.WriteColumnsln(row)
+				excellogger.WriteColumnsln(row)
 			}
 		}
 	}
